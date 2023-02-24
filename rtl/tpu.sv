f25a2d0c6615cbd2c54b7c057351023d5e594446
Sumguy31 <mnguyen2@andrew.cmu.edu>
2023-02-23 22:54:39 -0500

    moved files to rtl\/

--- rtl/tpu.sv
+++ rtl/tpu.sv
@@ -1,14 +1,14 @@
 `default_nettype none
+`include "defines.vh"
 
 module tpu
-#(parameter DATA_WIDTH=8,
-  parameter MATRIX_DIM=16,
+#(parameter MATRIX_DIM=16,
   parameter CONV_DIM=3)
  (input  logic clk, rst,
   input  logic insert_kernal, insert_matrix, ready,
-  input  logic [DATA_WIDTH-1:0] data_in,
+  input  data_t data_in,
   output logic done,
-  output logic [DATA_WIDTH-1:0] data_out);
+  output data_t data_out);
 
   typedef struct packed {
     logic [$clog2(MATRIX_DIM)-1:0] x,y;
@@ -23,36 +23,43 @@ module tpu
   } conv_coord_t;
 
   // KERNAL LOGIC
-  logic [CONV_DIM-1:0][CONV_DIM-1:0][DATA_WIDTH-1:0] kernal_data;
+  // HACK: Yosys doesn't support packed arrays https://github.com/YosysHQ/yosys/issues/340
+  data_t [(CONV_DIM*CONV_DIM)-1:0] kernal_data;
   conv_coord_t kernal_addr;
-  logic [CONV_DIM-1:0][CONV_DIM-1:0] kernal_we;
+  logic [(CONV_DIM*CONV_DIM)-1:0] kernal_we;
+  logic [($clog2(CONV_DIM*CONV_DIM))-1:0] kernal_data_sel;
   always_comb begin
     kernal_we = 'b0;
-    kernal_we[kernal_addr.x][kernal_addr.y] = insert_kernal;
+    kernal_data_sel = kernal_addr.x * CONV_DIM
+                      + {{$clog2(CONV_DIM){1'b0}},kernal_addr.y};
+    kernal_we[kernal_data_sel] = insert_kernal;
   end
   generate
     for (genvar i = 0; i < CONV_DIM; i++) begin : outer_loop_kernal
       for (genvar j = 0; j < CONV_DIM; j++) begin : inner_loop_kernal
-        register #(DATA_WIDTH) kernal_reg(.clk, .rst, .we(kernal_we[i][j]),
-                                          .D(data_in), .Q(kernal_data[i][j]));
+        register #(`DATA_WIDTH) kernal_reg(.clk, .rst, .we(kernal_we[(i * CONV_DIM) + j]),
+                                          .D(data_in), .Q(kernal_data[(i * CONV_DIM) + j]));
       end
     end
   endgenerate
 
   // MATRIX LOGIC
-  logic [MATRIX_DIM-1:0][MATRIX_DIM-1:0][DATA_WIDTH-1:0] matrix_data;
+  // HACK: Yosys doesn't support packed arrays https://github.com/YosysHQ/yosys/issues/340
+  data_t [(MATRIX_DIM*MATRIX_DIM)-1:0] matrix_data;
   matrix_coord_t matrix_addr;
-  logic [MATRIX_DIM-1:0][MATRIX_DIM-1:0] matrix_we;
+  logic [(MATRIX_DIM*MATRIX_DIM)-1:0] matrix_we;
+  logic [($clog2(MATRIX_DIM*MATRIX_DIM))-1:0] matrix_data_sel;
   always_comb begin
     matrix_we = 'b0;
-    matrix_we[matrix_addr.x[$clog2(MATRIX_DIM)-1:0]]
-             [matrix_addr.y[$clog2(MATRIX_DIM)-1:0]]  = insert_matrix;
+    matrix_data_sel = matrix_addr.x[$clog2(MATRIX_DIM)-1:0] *  MATRIX_DIM
+                    + {{$clog2(MATRIX_DIM){1'b0}}, matrix_addr.y[$clog2(MATRIX_DIM)-1:0]};
+    matrix_we[matrix_data_sel] = insert_matrix;
   end
   generate
     for (genvar i = 0; i < MATRIX_DIM; i++) begin : outer_loop_matrix
       for (genvar j = 0; j < MATRIX_DIM; j++) begin : inner_loop_matrix
-        register #(DATA_WIDTH) matrix_reg(.clk, .rst, .we(matrix_we[i][j]),
-                                          .D(data_in), .Q(matrix_data[i][j]));
+        register #(`DATA_WIDTH) matrix_reg(.clk, .rst, .we(matrix_we[i * MATRIX_DIM + j]),
+                                          .D(data_in), .Q(matrix_data[i * MATRIX_DIM + j]));
       end
     end
   endgenerate
@@ -82,10 +89,10 @@ module tpu
   assign mac_rst = base_x_incr & ready;
   assign done = mac_rst;
   assign mac_en = 1'b1; // TODO:
-  mac #(DATA_WIDTH,DATA_WIDTH)
+  mac #(`DATA_WIDTH,`DATA_WIDTH)
       conv_mac (.clk, .rst(mac_rst), .en(mac_en),
-                .a(kernal_data[kernal_addr.x][kernal_addr.y]),
-                .b(matrix_data[matrix_addr.x][matrix_addr.y]),
+                .a(kernal_data[kernal_data_sel]),
+                .b(matrix_data[matrix_data_sel]),
                 .sum(data_out));
 
 endmodule : tpu
