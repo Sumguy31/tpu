5dbe50fd88d87bd446689dcefdf1fd2feaf1c318
Sumguy31 <mnguyen2@andrew.cmu.edu>
2023-05-05 22:56:29 -0400

    integration with workflows + info.yaml update

--- src/chip.sv
+++ src/chip.sv
@@ -6,42 +6,14 @@ module my_chip (
     input logic clock,
     input logic reset // Important: Reset is ACTIVE-HIGH
 );
-    
-    // Basic counter design as an example
 
-
-    wire [6:0] led_out;
-    assign io_out[6:0] = led_out;
-
-    // external clock is 1000Hz, so need 10 bit counter
-    reg [9:0] second_counter;
-    reg [3:0] digit;
-
-    always @(posedge clock) begin
-        // if reset, set counter to 0
-        if (reset) begin
-            second_counter <= 0;
-            digit <= 0;
-        end else begin
-            // if up to 16e6
-            if (second_counter == 1000) begin
-                // reset
-                second_counter <= 0;
-
-                // increment digit
-                digit <= digit + 1'b1;
-
-                // only count from 0 to 9
-                if (digit == 9)
-                    digit <= 0;
-
-            end else
-                // increment counter
-                second_counter <= second_counter + 1'b1;
-        end
-    end
-
-    // instantiate segment display
-    seg7 seg7(.counter(digit), .segments(led_out));
+  tpu TPU(.clk(clock), .rst(reset),
+          .insert_kernal(io_in[0]),
+          .insert_matrix(io_in[1]),
+          .ready(io_in[2]),
+          .data_in(io_in[11:4]),
+          .done(io_out[0]),
+          .data_out(io_out[11:4])
+         );
 
 endmodule
